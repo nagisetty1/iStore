d574e5d3751522d08457037a5b7e6fa2bc6e6070
NagiSetty.Akuthota <NagiSetty.Akuthota@ivycomptech.com>
2022-10-29 12:46:27 +0530

    temp login implemented

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -4,16 +4,41 @@ namespace iStore;
 
 public partial class MainPage : ContentPage
 {
+    #region fields
+    private string loginResult;
+	private bool loginButtonVisibility;
+	private string displayName;
+	private bool displayNameVisibility;
+	#endregion
+
+	#region Properties
+	public bool LoginButtonVisibility { get => loginButtonVisibility; set { loginButtonVisibility = value; OnPropertyChanged(); } }
+	public string DisplayName { get => displayName; set { displayName = value; OnPropertyChanged(); } }
+	public bool DisplayNameVisibility { get => displayNameVisibility; set { displayNameVisibility = value; OnPropertyChanged(); } }
+
+	#endregion
 
 	public MainPage()
 	{
 		InitializeComponent();
-		BindingContext = this;
 		//MauiPopup.PopupAction.DisplayPopup(new SignInPopup());
+		LoginButtonVisibility = true;
+		loginResult = string.Empty;
+		DisplayName = string.Empty;
+		BindingContext = this;
+		DisplayNameVisibility = false;
 	}
 
 	public ICommand RegisterCommand => new Command<string>(async (parameter) => await Launcher.OpenAsync(parameter));
 
-	public ICommand LoginButtonCommand => new Command<string>((parameter) => MauiPopup.PopupAction.DisplayPopup(new SignInPopup()));
+	public ICommand LoginButtonCommand => new Command<string>(async (parameter) => { 
+		loginResult = await MauiPopup.PopupAction.DisplayPopup(new SignInPopup());
+        if (!string.IsNullOrEmpty(loginResult))
+        {
+			LoginButtonVisibility = false;
+			DisplayNameVisibility = true;
+			DisplayName = loginResult;
+		}
+	});
 }
 
--- SignInPopup.xaml.cs
+++ SignInPopup.xaml.cs
@@ -11,6 +11,28 @@ public partial class SignInPopup : BasePopupPage
 		BindingContext = this;
 	}
 
-	public ICommand LoginButtonCommand => new Command<string>((parameter) => MauiPopup.PopupAction.ClosePopup());
+	#region fields
+	private string userName;
+	private string password;
+	#endregion
+
+	#region Properties
+	public string UserName { get => userName; set { userName = value; OnPropertyChanged(); } }
+	public string Password { get => password; set { password = value; OnPropertyChanged(); } }
+	#endregion
+
+	public ICommand LoginButtonCommand => new Command<string>((parameter) => {
+		//TODO: Validate credentials and send it caller.
+		if (UserName == "ns" && Password == "ns")
+		{
+			MauiPopup.PopupAction.ClosePopup(UserName);
+		}
+		else
+        {
+			MauiPopup.PopupAction.ClosePopup();
+		}
+		});
 	public ICommand CloseButtonCommand => new Command<string>((parameter) => MauiPopup.PopupAction.ClosePopup());
+
+    
 }
